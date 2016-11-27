dcdbbf6591d15d14f24d8e4e46960a129d04969c
Wisertown <aaron.wise253@gmail.com>
2016-11-27 13:56:05 -0500

    first commit

---
+++ TriviaFacts/ColorModel.swift
@@ -0,0 +1,9 @@
+//
+//  ColorModel.swift
+//  TriviaFacts
+//
+//  Created by Aaron on 11/27/16.
+//  Copyright © 2016 Aaron. All rights reserved.
+//
+
+import Foundation
---
+++ TriviaFacts/FactModel.swift
@@ -0,0 +1,9 @@
+//
+//  FactModel.swift
+//  TriviaFacts
+//
+//  Created by Aaron on 11/5/16.
+//  Copyright © 2016 Aaron. All rights reserved.
+//
+
+import Foundation
